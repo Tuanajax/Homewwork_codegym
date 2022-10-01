e613fdc95f85f14969952a768c9208c213880c2c
Tuanajax <110762160+Tuanajax@users.noreply.github.com>
2022-10-01 15:35:22 +0700

    Update 3.Current_ex.py
    Chuyển đổi USB sang VND

--- 3.Current_ex.py
+++ 3.Current_ex.py
@@ -2,3 +2,4 @@ amount = int(input("Enter amount of money in dollar: "))
 rate = int(input("Enter The EX_rate (VND/USD)): "))
 VN_dong = amount * rate
 print("so,{} dollar into {} VND.".format(amount,VN_dong) )
+# ok
