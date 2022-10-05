f184a3ef1975d037d5e58d23c69c6b18dd31f37e
tuanvu <vutuan190294@gmail.com>
2022-10-05 15:40:48 +0700

    ve ngoi nha mo uoc

---
+++ House/16.module_house.py
@@ -0,0 +1,3 @@
+import Tuna as AT
+AT.rec(100,200)
+AT.tri(100,60)
---
+++ House/Tuna.py
@@ -0,0 +1,32 @@
+from math import cos, radians
+import turtle
+Tuan_Scr = turtle.Screen()
+Tuan_br = turtle.Turtle()
+def rec(l,w):
+    ''' draw rectangle
+    l(float): length
+    w(float): width'''
+    Tuan_br.pendown()
+    Tuan_br.fillcolor("red")
+    Tuan_br.begin_fill()
+    Tuan_br.fd(l)
+    Tuan_br.left(90)
+    Tuan_br.fd(w)
+    Tuan_br.left(90)
+    Tuan_br.fd(l)
+    Tuan_br.left(90)
+    Tuan_br.fd(w)
+    Tuan_br.end_fill()
+    Tuan_br.penup()
+
+def tri(b,a):
+    ''' Draw triangle
+    b(float): base
+    a(degree): angle'''
+    Tuan_br.pendown()
+    Tuan_br.fd(b)
+    h = 0.5*b/cos(radians(a))
+    Tuan_br.left(180-a)
+    Tuan_br.fd(h) 
+    Tuan_br.left(2*a)
+    Tuan_br.fd(h) 
